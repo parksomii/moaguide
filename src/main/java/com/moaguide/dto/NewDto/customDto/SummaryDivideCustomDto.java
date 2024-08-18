3e3816cf360c94f1192908e12075619bd9745281
chanho <jincanhao@naver.com>
2024-08-18 23:20:15 +0900

    요약 배당금 수정 완료2

--- src/main/java/com/moaguide/dto/NewDto/customDto/SummaryDivideCustomDto.java
+++ src/main/java/com/moaguide/dto/NewDto/customDto/SummaryDivideCustomDto.java
@@ -13,13 +13,11 @@ public class SummaryDivideCustomDto {
     private String category;
     private String name;
     private double dividend;
-    private double dividendRate;
 
     public SummaryDivideCustomDto(Divide findDivide) {
         this.product_Id = findDivide.getProductId();
         this.category = findDivide.getCategory();
         this.name = findDivide.getName();
         this.dividend = findDivide.getDividend();
-        this.dividendRate = findDivide.getDividendRate();
     }
 }
