3f4c9d59a6e64f4e4eb6d26af793417be912f96d
kch12 <gaemkyu4@gmail.com>
2024-08-19 21:40:07 +0900

    리포트 수정 완료

--- src/main/java/com/moaguide/domain/report/ReportRepository.java
+++ src/main/java/com/moaguide/domain/report/ReportRepository.java
@@ -10,32 +10,43 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
-    // 최신 리포트
-    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id,r.title,r.category,r.date) FROM Report r ORDER BY r.date DESC")
+    // 메인 최신 리포트
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id,r.title, r.content, r.category,r.date) FROM Report r ORDER BY r.date DESC")
     List<ReportCustomDto> findLatest(Pageable pageable);
 
     // 카테고리별 최신 리포트
-    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id,r.title,r.category,r.date) FROM Report r WHERE r.category = :category ORDER BY r.date DESC")
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id,r.title,r.content,r.category,r.date) FROM Report r WHERE r.category = :category ORDER BY r.date DESC")
     List<ReportCustomDto> findCategoryLatest(@Param("category") String category, Pageable pageable);
 
-    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id,r.title,r.category,r.date) FROM Report r WHERE r.category = :category AND (r.subCategory = 'situation' OR r.subCategory = 'guide' OR r.subCategory = :subcategory) ORDER BY r.date DESC")
+    // 카테고리별 디테일 리포트 조회
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id,r.title,r.content,r.category,r.date) FROM Report r WHERE r.category = :category AND (r.subCategory = 'situation' OR r.subCategory = 'guide' OR r.subCategory = :subcategory) ORDER BY r.date DESC")
     Page<ReportCustomDto> findBydetail(@Param("category") String category, @Param("subcategory") String subCategory, Pageable pageable);
 
+    // 리포트 조회수
     Report findById(int reportId);
+
     // 리포트 상세
-    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id, r.title, r.category, r.date) " +
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id, r.title, r.content, r.category, r.date) " +
             "FROM Report r WHERE r.id = :reportId")
     ReportCustomDto findReportDetail(@Param("reportId") int reportId);
 
-    // 인기순
-    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id, r.title, r.category, r.date) " +
+    // 전체 카테고리 인기순
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id, r.title, r.content, r.category, r.date) " +
+            "FROM Report r WHERE r.subCategory = :subcategory ORDER BY r.view DESC")
+    Page<ReportCustomDto> findAllByViews(@Param("subcategory") String subcategory, Pageable pageable);
+    // 카테고리별 인기순
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id, r.title, r.content, r.category, r.date) " +
             "FROM Report r WHERE r.category = :category AND r.subCategory = :subcategory " +
             "ORDER BY r.view DESC")
-    Page<ReportCustomDto> findAllByViews(@Param("category") String category, @Param("subcategory") String subcategory, Pageable pageable);
+    Page<ReportCustomDto> findListByAllbyViews(@Param("category") String category, @Param("subcategory") String subcategory, Pageable pageable);
 
-    // 최신순
-    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id, r.title, r.category, r.date) " +
+    // 전체 카테고리 최신순
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id, r.title, r.content, r.category, r.date) " +
+            "FROM Report r WHERE r.subCategory = :subcategory ORDER BY r.date DESC")
+    Page<ReportCustomDto> findAllbyLatest(@Param("subcategory") String subcategory, Pageable pageable);
+    // 카테고리별 최신순
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.ReportCustomDto(r.id, r.title, r.content, r.category, r.date) " +
             "FROM Report r WHERE r.category = :category AND r.subCategory = :subcategory " +
             "ORDER BY r.date DESC")
-    Page<ReportCustomDto> findAllByLatest(@Param("category") String category, @Param("subcategory") String subcategory, Pageable pageable);
+    Page<ReportCustomDto> findListByAllbyLatest(@Param("category") String category, @Param("subcategory") String subcategory, Pageable pageable);
 }
--- src/main/java/com/moaguide/service/ReportService.java
+++ src/main/java/com/moaguide/service/ReportService.java
@@ -21,36 +21,47 @@ import java.util.List;
 public class ReportService {
     private final ReportRepository reportRepository;
 
+    // 디테일 리포트
     public Page<ReportCustomDto> findBylist(String category, String subCategory, Pageable pageable) {
         Page<ReportCustomDto> reportCustomDtos = reportRepository.findBydetail(category,subCategory,pageable);
         return reportCustomDtos;
     }
 
+    // 메인 리포트
     public List<ReportCustomDto> getMainReport(Pageable pageable) {
         List<ReportCustomDto> reportCustomDtos = reportRepository.findLatest(pageable);
         return reportCustomDtos;
     }
 
+    // 요약 관련 리포트
     public List<ReportCustomDto> getSummary(String category, Pageable pageable) {
         List<ReportCustomDto> reportCustomDtos = reportRepository.findCategoryLatest(category, pageable);
         return reportCustomDtos;
     }
 
+    // 리포트 조회수
     public Report findById(int reportId) {
         return reportRepository.findById(reportId);
     }
 
+    // 리포트 상세
     public ReportCustomDto getReportDetail(int reportId) {
         return reportRepository.findReportDetail(reportId);
     }
 
     // 인기순
-    public Page<ReportCustomDto> getAllPopularBySubCategory(String category, String subcategory, Pageable pageable) {
-        return reportRepository.findAllByViews(category, subcategory, pageable);
+    public Page<ReportCustomDto> getAllPopularBySubCategory(String category, String subcategory, int page, int size) {
+        if (category.equals("all")) {
+            return reportRepository.findAllByViews(subcategory, PageRequest.of(page - 1, size));
+        }
+        return reportRepository.findListByAllbyViews(category, subcategory, PageRequest.of(page - 1, size));
     }
 
     // 최신순
-    public Page<ReportCustomDto> getAllLatestBySubCategory(String category, String subcategory, Pageable pageable) {
-        return reportRepository.findAllByLatest(category, subcategory, pageable);
+    public Page<ReportCustomDto> getAllLatestBySubCategory(String category, String subcategory, int page, int size) {
+        if (category.equals("all")) {
+            return reportRepository.findAllbyLatest(subcategory, PageRequest.of(page - 1, size, Sort.by("date").descending()));
+        }
+        return reportRepository.findListByAllbyLatest(category, subcategory, PageRequest.of(page - 1, size, Sort.by("date").descending()));
     }
 }
