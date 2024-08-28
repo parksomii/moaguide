e96ea768133e63063bb2ca7999fe4e78df8ce33f
LJH <qufwnsgh3@naver.com>
2024-08-28 16:34:48 +0900

    검색 점검 11

--- src/main/java/com/moaguide/dto/SearchRankDto.java
+++ src/main/java/com/moaguide/dto/SearchRankDto.java
@@ -1,8 +1,10 @@
 package com.moaguide.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class SearchRankDto {
     private String keyword;
     private int rank;
