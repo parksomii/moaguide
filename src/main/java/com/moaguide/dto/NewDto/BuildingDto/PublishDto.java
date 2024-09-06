cbf23f261eb02a805013c938ac7f89fdb4f1f90f
LJH <qufwnsgh3@naver.com>
2024-09-06 11:53:08 +0900

    대략 수정

--- src/main/java/com/moaguide/dto/NewDto/BuildingDto/PublishDto.java
+++ src/main/java/com/moaguide/dto/NewDto/BuildingDto/PublishDto.java
@@ -24,22 +24,6 @@ public class PublishDto {
         this.publisher = getPublisher();
         this.last_divide = dividend;
         this.piece = buildingDetail.getProductId().getPiece();
-        // 발행액과 증권 수를 곱한 값 계산
-        Long total = (long) buildingDetail.getIssuanceValue() * getPiece();
-
-        // 값이 100,000,000 이상이면 "억원", 10,000 이상이면 "만원", 그 이하면 "원" 붙이기
-        if (total >= 100000000) {
-            // 100,000,000으로 나눈 후 소수점 두 자리까지 표현
-            BigDecimal totalInBillions = new BigDecimal(total).divide(new BigDecimal(100000000), 2, RoundingMode.HALF_UP);
-            this.totalPrice = totalInBillions + "억원";
-        } else if (total >= 10000) {
-            // 10,000으로 나눈 후 소수점 두 자리까지 표현
-            BigDecimal totalInTenThousands = new BigDecimal(total).divide(new BigDecimal(10000), 2, RoundingMode.HALF_UP);
-            this.totalPrice = totalInTenThousands + "만원";
-        } else {
-            // 10,000 미만인 경우
-            this.totalPrice = total + "원";
-        }
         this.subscription = buildingDetail.getProductId().getName();
         this.listingDate = getListingDate();
     }
