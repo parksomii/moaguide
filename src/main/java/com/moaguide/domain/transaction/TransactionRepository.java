79a2b595784c4b52e34db2bcf55947ecd9347d10
LJH <qufwnsgh3@naver.com>
2024-09-06 23:05:37 +0900

    리스트 완료 6

--- src/main/java/com/moaguide/domain/transaction/TransactionRepository.java
+++ src/main/java/com/moaguide/domain/transaction/TransactionRepository.java
@@ -14,23 +14,10 @@ import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    @Query("SELECT t FROM Transaction t WHERE t.productId.productId = :productId ORDER BY t.tradeDay DESC")
-    List<Transaction> findFirstByProductId(@Param("productId") String productId, Pageable pageable);
-
-    @Procedure(procedureName = "getRecentTransactions")
-    List<Transaction> findAllByProductIdAndTradeDayAfter(@Param("productId") String productId);
-
-    @Query("SELECT t FROM Transaction t WHERE t.productId.productId = :productId order by t.tradeDay desc")
-    List<Transaction> findTwoByProductId(@Param("productId")String id,Pageable pageable);
 
     // 일별 거래내역 조회
     @Query("SELECT new com.moaguide.dto.NewDto.customDto.TransactionDto(t.tradeDay,t.price) FROM Transaction t WHERE t.productId.productId = :productId AND t.tradeDay >= :day order by t.tradeDay")
     List<TransactionDto> findbyday(@Param("productId") String productId, @Param("day")LocalDate date);
 
-    // 최근 거래내역 조회
-    @Query("SELECT t FROM Transaction t WHERE t.productId.productId = :productId order by t.tradeDay desc")
-    Page<Transaction> findByProductId(@Param("productId") String productId, Pageable pageable);
 
-    @Procedure(procedureName = "GetLatestProductsByPrice")
-    List<Transaction> findLatestTransactionsByPrice(@Param("productId") String productId, Pageable pageable);
 }
--- src/main/java/com/moaguide/service/TransactionService.java
+++ src/main/java/com/moaguide/service/TransactionService.java
@@ -24,62 +24,6 @@ import java.util.List;
 public class TransactionService {
     private final TransactionRepository transactionRepository;
 
-    public List<Transaction> findAllByid(List<SummaryDto> summaryList) {
-        log.info("*************** TransactionService findAllByid");
-        log.info("*************** TransactionService findAllByid - summaryList : {}", summaryList);
-        List<Transaction> transactions = new ArrayList<>();
-        Pageable pageable = PageRequest.of(0, 1); // 첫 번째 결과만 반환
-        for (SummaryDto summary : summaryList) {
-            String productId = summary.getProductId();
-            // 첫 번째 Transaction만 가져오기 위해 Pageable 사용
-            List<Transaction> result = transactionRepository.findFirstByProductId(productId, pageable);
-            if (!result.isEmpty()) {
-                transactions.add(result.get(0)); // 리스트에서 첫 번째 항목을 추가
-            }
-        }
-
-        log.info("Transactions found: {}", transactions);
-        return transactions;
-    }
-
-    @Transactional
-    public List<List<Transaction>> graph(List<SummaryDto> summaryList) {
-        List<List<Transaction>> transactions = new ArrayList<>();
-        for (SummaryDto summary : summaryList) {
-            String productId = summary.getProductId();
-            List<Transaction> transactionList = transactionRepository.findAllByProductIdAndTradeDayAfter(summary.getProductId());
-            if (!transactionList.isEmpty()) {
-                transactions.add(transactionList);
-            } else {
-                transactions.add(null);
-            }
-        }
-        log.info(transactions.toString());
-        return transactions;
-    }
-
-    @Transactional
-    public List<Transaction> findLatestTransactionsByPrice(String id, Pageable pageable) {
-        log.info("*************** TransactionService findLatestTransactionsByPrice");
-        List<Transaction> transactions = transactionRepository.findLatestTransactionsByPrice(id, pageable);
-        log.info("Transactions found: {}", transactions);
-        return transactions;
-    }
-
-    public List<Transaction> findtwoByproductId(String id) {
-        Pageable pageable = PageRequest.of(0, 2);
-        List<Transaction> transactions = transactionRepository.findTwoByProductId(id,pageable);
-        return transactions;
-    }
-
-    public Transaction findbyproductId(String id) {
-        Pageable pageable = PageRequest.of(0, 1);
-        Page<Transaction> transaction = transactionRepository.findByProductId(id, pageable);
-        return transaction.getContent().get(0); // 첫 번째 Transaction 반환
-    }
-
-
-
     public List<TransactionDto> findbymonth(String productId, int month) {
         List<TransactionDto> transactionDtos = transactionRepository.findbyday(productId,LocalDate.now().minusMonths(month));
         return transactionDtos;
