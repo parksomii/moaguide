5704400b6c36b011a816f6eb277dbdd3bc77f501
LJH <qufwnsgh3@naver.com>
2024-09-09 18:05:43 +0900

    빌딩쪽 전체 수정완료 35 https://api.moaguide.com/detail/building/base/sou.8 완료 https://api.moaguide.com/detail/building/sub/sou.8 완료 https://api.moaguide.com/detail/building/area/sou.8부분 체크 중 https://api.moaguide.com/detail/building/population/sou.8부분 체크 중

--- src/main/java/com/moaguide/domain/building/population/PopulationRepository.java
+++ src/main/java/com/moaguide/domain/building/population/PopulationRepository.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 public interface PopulationRepository extends JpaRepository<Population, Long> {
 
-    @Procedure(name = "populate")
     @Query("select new com.moaguide.dto.NewDto.BuildingDto.PopulationDto(p.weekDay ,p.total ,p.age0 ,p.age10 ,p.age20,p.age30,p.age40,p.age50,p.age60,p.age70,p.man,p.girl )" +
             "FROM Population p,BuildingDetail b " +
             "where p.districts.districtsId = b.districtsId.districtsId and p.day = :in_day and b.productId.productId = :id ")
