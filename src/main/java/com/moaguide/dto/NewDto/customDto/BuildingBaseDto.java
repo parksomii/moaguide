1e08def389d4eea86a955f6dd4293eb9c5aa7dda
LJH <qufwnsgh3@naver.com>
2024-09-09 13:06:20 +0900

    빌딩쪽 전체 수정완료 19

--- src/main/java/com/moaguide/dto/NewDto/customDto/BuildingBaseDto.java
+++ src/main/java/com/moaguide/dto/NewDto/customDto/BuildingBaseDto.java
@@ -3,10 +3,12 @@ package com.moaguide.dto.NewDto.customDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class BuildingBaseDto {
     private String name;
 
