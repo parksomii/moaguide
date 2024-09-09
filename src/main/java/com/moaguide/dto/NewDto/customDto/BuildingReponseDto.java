7422bd43b35c6cc24f83b710e84e53676bdf8ac5
LJH <qufwnsgh3@naver.com>
2024-09-10 00:54:44 +0900

    빌딩쪽 전체 수정완료 5q

--- src/main/java/com/moaguide/dto/NewDto/customDto/BuildingReponseDto.java
+++ src/main/java/com/moaguide/dto/NewDto/customDto/BuildingReponseDto.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
+import java.beans.ConstructorProperties;
+
 @Getter
 @NoArgsConstructor
 @Setter
@@ -21,4 +22,18 @@ public class BuildingReponseDto {
     private Double dividend;
     private Double dividendRate;
     private Integer divideCycle;
+
+    @ConstructorProperties({"productId", "category", "platform", "name", "price", "priceRate", "totalPrice", "dividend", "dividendRate", "divideCycle"})
+    public BuildingReponseDto(String productId, String category, String platform, String name, Integer price, Double priceRate, Long totalPrice, Double dividend, Double dividendRate, Integer divideCycle) {
+        this.productId = productId;
+        this.category = category;
+        this.platform = platform;
+        this.name = name;
+        this.price = price;
+        this.priceRate = priceRate;
+        this.totalPrice = totalPrice;
+        this.dividend = dividend;
+        this.dividendRate = dividendRate;
+        this.divideCycle = divideCycle;
+    }
 }
