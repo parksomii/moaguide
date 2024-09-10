b76c46e21d298e8e3d8984c13cada0ca2e6eceb1
chanho <jincanhao@naver.com>
2024-09-10 20:08:31 +0900

    미술, 한우 카테고리 수정 3

--- src/main/java/com/moaguide/domain/hanwoo/Grade1RateRepository.java
+++ src/main/java/com/moaguide/domain/hanwoo/Grade1RateRepository.java
@@ -8,10 +8,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface Grade1RateRepository extends JpaRepository<Grade1Rate, Long> {
-    @Query("SELECT new com.moaguide.dto.NewDto.customDto.Grade1RateDto(MONTH(g.prdDe), AVG(g.prdDt), g.cType) " +
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.Grade1RateDto(MONTH(g.prdDe), AVG(g.prdDt)) " +
             "FROM Grade1Rate g " +
             "WHERE YEAR(g.prdDe) = :year " +
-            "GROUP BY MONTH(g.prdDe), g.cType")
+            "GROUP BY MONTH(g.prdDe)")
     List<Grade1RateDto> findGrade1Rate(@Param("year") int year);
 
 }
--- src/main/java/com/moaguide/dto/NewDto/customDto/ProductionCostDto.java
+++ src/main/java/com/moaguide/dto/NewDto/customDto/ProductionCostDto.java
@@ -6,13 +6,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class ProductionCostDto {
-    private String cType; // 카테고리 타입
     private Integer year; // 월
     private Double productionCost; // 1등급 비율
 
     // 생성자
-    public ProductionCostDto(Integer year, Double productionCost, String cType) {
-        this.cType = cType;
+    public ProductionCostDto(Integer year, Double productionCost) {
         this.year = year;
         this.productionCost = productionCost;
     }
