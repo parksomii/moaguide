ca53a5adb53f1313f945371371f44bb6bbf48a75
chanho <jincanhao@naver.com>
2024-09-20 14:19:50 +0900

    음악, 콘텐츠 카테고리 수정 15

--- src/main/java/com/moaguide/controller/MusicRestController.java
+++ src/main/java/com/moaguide/controller/MusicRestController.java
@@ -3,15 +3,17 @@ package com.moaguide.controller;
 import com.moaguide.dto.NewDto.MusicBaseResponseDto;
 import com.moaguide.dto.NewDto.MusicSubResponseDto;
 import com.moaguide.dto.NewDto.customDto.*;
+import com.moaguide.dto.NewDto.musicDto.ConsertDto;
+import com.moaguide.dto.NewDto.musicDto.SearchDto;
+import com.moaguide.dto.NewDto.musicDto.SteamingDto;
+import com.moaguide.dto.NewDto.musicDto.ViewDto;
 import com.moaguide.service.MusicDetailService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -38,7 +40,35 @@ public class MusicRestController {
 
 /*    @GetMapping("sub/{product_Id}")
     public ResponseEntity<Object> Sub(@PathVariable String product_Id) {
-
+    MusicYoutubeDto music = musicService.findYoutube(product_Id);
         return ResponseEntity.ok(new MusicSubResponseDto());
     }*/
+
+    @GetMapping("view/{product_Id}")
+    public ResponseEntity<?> view(@PathVariable String product_Id, @RequestParam String date) {
+        // 스트리밍 수 (6개월, 1년, 3년, 전체)
+        List<ViewDto> music = musicService.findView(product_Id, date);
+        return ResponseEntity.ok(music);
+    }
+
+    @GetMapping("search/{product_Id}")
+    public ResponseEntity<?> search(@PathVariable String product_Id, @RequestParam String date) {
+        // 검색량 (일주일, 6개월, 1년, 전체)
+        List<SearchDto> music = musicService.findSearch(product_Id, date);
+        return ResponseEntity.ok(music);
+    }
+
+    @GetMapping("streaming/{product_Id}")
+    public ResponseEntity<?> streaming(@PathVariable String product_Id, @RequestParam String date) {
+        // 스트리밍 수 (일주일, 6개월, 1년, 전체)
+        List<SteamingDto> music = musicService.findStreaming(product_Id, date);
+        return ResponseEntity.ok(music);
+    }
+
+    @GetMapping("consert/{product_Id}")
+    public ResponseEntity<?> consert(@PathVariable String product_Id) {
+        // 공연일정 최신순
+        List<ConsertDto> music = musicService.findConsert(product_Id);
+        return ResponseEntity.ok(music);
+    }
 }
--- src/main/java/com/moaguide/dto/NewDto/MusicSubResponseDto.java
+++ src/main/java/com/moaguide/dto/NewDto/MusicSubResponseDto.java
@@ -1,8 +1,14 @@
 package com.moaguide.dto.NewDto;
 
+import com.moaguide.dto.NewDto.musicDto.ConsertDto;
+import com.moaguide.dto.NewDto.musicDto.SearchDto;
+import com.moaguide.dto.NewDto.musicDto.SteamingDto;
+import com.moaguide.dto.NewDto.musicDto.ViewDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,11 +16,11 @@ import lombok.NoArgsConstructor;
 public class MusicSubResponseDto {
     private String youtubeUrl;  // 유튜브 url
     // 유튜브 조회수 리스트
-    private String youtubeViewCount; // 유튜브 조회수
+    private List<ViewDto> youtubeViewCount; // 유튜브 조회수
     // 검색량 리스트
-    private String searchVolume;    // 검색량
+    private List<SearchDto> searchVolume;    // 검색량
     // 스트리밍 수 리스트
-    private String streamingCount;  // 스트리밍 수
+    private List<SteamingDto> streamingCount;  // 스트리밍 수
     // 공연일정 리스트
-    private String performanceSchedule;  // 공연일정
+    private List<ConsertDto> consert;  // 공연일정
 }
