3a63388fa5bf165e9a127a62d7cb863131cfae67
LJH <qufwnsgh3@naver.com>
2024-09-20 11:51:42 +0900

    오류수정

--- src/main/java/com/moaguide/domain/detail/ContentRepository.java
+++ src/main/java/com/moaguide/domain/detail/ContentRepository.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
-    @Procedure(procedureName = "GetContentDetails")
+    @Query(value = "CALL GetContentDetails(:productId)", nativeQuery = true)
     ContentDetailDto findByDetail(@Param("Id") String productId);
 
     @Procedure(procedureName = "GetContentpublish")
