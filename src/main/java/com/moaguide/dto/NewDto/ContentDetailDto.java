89c186c34fde2a500e1f7e6ab0449abe2a88158b
LJH <qufwnsgh3@naver.com>
2024-09-20 14:51:18 +0900

    오류수정 9

--- src/main/java/com/moaguide/dto/NewDto/ContentDetailDto.java
+++ src/main/java/com/moaguide/dto/NewDto/ContentDetailDto.java
@@ -1,5 +1,8 @@
 package com.moaguide.dto.NewDto;
 
+import jakarta.persistence.ColumnResult;
+import jakarta.persistence.ConstructorResult;
+import jakarta.persistence.SqlResultSetMapping;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +12,23 @@ import java.sql.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@SqlResultSetMapping(
+        name = "ContentDetailDtoMapping",
+        classes = @ConstructorResult(
+                targetClass = ContentDetailDto.class,
+                columns = {
+                        @ColumnResult(name = "productId", type = String.class),
+                        @ColumnResult(name = "name", type = String.class),
+                        @ColumnResult(name = "genre", type = String.class),
+                        @ColumnResult(name = "category", type = String.class),
+                        @ColumnResult(name = "platform", type = String.class),
+                        @ColumnResult(name = "totalPrice", type = long.class),
+                        @ColumnResult(name = "rate", type = Double.class),
+                        @ColumnResult(name = "date", type = Date.class),
+                        @ColumnResult(name = "lowPrice", type = Integer.class)
+                }
+        )
+)
 public class ContentDetailDto {
     private String productId;
     private String name;
