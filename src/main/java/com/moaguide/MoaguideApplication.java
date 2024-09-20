2dbd732570e52121d69b852e865688a606bc64ef
LJH <qufwnsgh3@naver.com>
2024-09-20 21:56:56 +0900

    오류 체크해보기 11

--- src/main/java/com/moaguide/MoaguideApplication.java
+++ src/main/java/com/moaguide/MoaguideApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.moaguide.controller")
 public class MoaguideApplication {
 
 
