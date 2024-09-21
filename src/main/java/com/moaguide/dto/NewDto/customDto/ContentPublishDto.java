afb70fdd9afca225bc5737fe9b86d5cdc875e81f
LJH <qufwnsgh3@naver.com>
2024-09-21 21:11:36 +0900

    오류 체크해보기 28

--- src/main/java/com/moaguide/dto/NewDto/customDto/ContentPublishDto.java
+++ src/main/java/com/moaguide/dto/NewDto/customDto/ContentPublishDto.java
@@ -15,8 +15,8 @@ public class ContentPublishDto {
     private String name;    // 상품명
     private String genre;   // 장르
     private String type;    // 증권종류
-    private String minAmount;   // 최소모집목표금액
-    private String maxAmount;   // 최대모집목표금액
+    private Long minAmount;   // 최소모집목표금액
+    private Long maxAmount;   // 최대모집목표금액
     private int piece;          // 최소 모집수량
     private int basePrice;      // 1주당 가격
     private Integer minInvestment;  // 최소 투자금액
