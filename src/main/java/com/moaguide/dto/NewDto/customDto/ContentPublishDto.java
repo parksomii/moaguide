bca3ddfaa5761e50767c334efb229d40f2bbb724
LJH <qufwnsgh3@naver.com>
2024-09-21 22:46:48 +0900

    오류 체크해보기 29

--- src/main/java/com/moaguide/dto/NewDto/customDto/ContentPublishDto.java
+++ src/main/java/com/moaguide/dto/NewDto/customDto/ContentPublishDto.java
@@ -17,8 +17,8 @@ public class ContentPublishDto {
     private String type;    // 증권종류
     private Long minAmount;   // 최소모집목표금액
     private Long maxAmount;   // 최대모집목표금액
-    private int piece;          // 최소 모집수량
-    private int basePrice;      // 1주당 가격
+    private Integer piece;          // 최소 모집수량
+    private Integer basePrice;      // 1주당 가격
     private Integer minInvestment;  // 최소 투자금액
     private Date issuanceDate; // 증권발행일
     private Date expirationDate;   // 증권만기일
