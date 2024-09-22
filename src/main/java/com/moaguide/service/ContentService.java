3b1ba1c74a7c5c9bc736e4d97d0cf6cc47469b5b
LJH <qufwnsgh3@naver.com>
2024-09-22 14:47:43 +0900

    로그 찍어서 오류 체크 47

--- src/main/java/com/moaguide/service/ContentService.java
+++ src/main/java/com/moaguide/service/ContentService.java
@@ -26,7 +26,7 @@ public class ContentService {
         return contentRepository.findByDetail(productId);
     }
 
-    @Transactional(readOnly = false)
+    @Transactional(propagation = Propagation.REQUIRES_NEW, readOnly = false)
     public ContentBaseDto findBase(String productId, String genre) {
         log.info("받은 상품명: {}", productId);
         if(genre.equals("MOVIE") || genre.equals("EXHIBITION") || genre.equals("CULTURE") || genre.equals("TRAVEL") ){
