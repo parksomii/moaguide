152ff211701e327eff8a89b800cef2f5a30ad1b2
LJH <qufwnsgh3@naver.com>
2024-09-26 17:55:55 +0900

    오류 수정

--- src/main/java/com/moaguide/domain/history/HistoryRepository.java
+++ src/main/java/com/moaguide/domain/history/HistoryRepository.java
@@ -3,6 +3,7 @@ package com.moaguide.domain.history;
 import com.moaguide.dto.NewDto.customDto.TransactionDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -12,5 +13,5 @@ import java.util.List;
 public interface HistoryRepository extends JpaRepository<History, Long> {
 
     @Query("SELECT new com.moaguide.dto.NewDto.customDto.TransactionDto(h.tradeDay,h.price) FROM History h WHERE h.productId.productId = :productId AND h.tradeDay >= :day order by h.tradeDay")
-    List<TransactionDto> findbyallday(String productId, LocalDate localDate);
+    List<TransactionDto> findbyallday(@Param("productId") String productId,@Param("tradeDay") LocalDate localDate);
 }
