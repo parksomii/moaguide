73e9a97742703ae32aade50c53f3bf0774e60887
LJH <qufwnsgh3@naver.com>
2024-09-30 15:28:41 +0900

    한우 수정

--- src/main/java/com/moaguide/dto/NewDto/HanwooMarketResponseDto.java
+++ src/main/java/com/moaguide/dto/NewDto/HanwooMarketResponseDto.java
@@ -8,14 +8,26 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class HanwooMarketResponseDto {
     private List<CattlePopulationDto> cattlePopulation; // 한우 사육두수
     private List<CattleSaleDto> cattleSale; // 한우 매각두수
     private List<CattleFarmDto> cattleFarm; // 한우사육농가수
     private List<CattleTransactionDto> cattleTransaction;   // 한우 거래정육량
+
+
+    public HanwooMarketResponseDto(List<CattlePopulationDto> cattlePopulation,
+                                   List<CattleSaleDto> cattleSale,
+                                   List<CattleFarmDto> cattleFarm,
+                                   List<CattleTransactionDto> cattleTransaction) {
+        this.cattlePopulation = cattlePopulation != null ? cattlePopulation : new ArrayList<>();
+        this.cattleSale = cattleSale != null ? cattleSale : new ArrayList<>();
+        this.cattleFarm = cattleFarm != null ? cattleFarm : new ArrayList<>();
+        this.cattleTransaction = cattleTransaction != null ? cattleTransaction : new ArrayList<>();
+    }
+
 }
