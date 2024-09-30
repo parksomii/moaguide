167a4d59c5ddf21cdb265e15b2f3f6b3ed903a25
LJH <qufwnsgh3@naver.com>
2024-09-30 13:36:15 +0900

    한우 날짜부분 수정

--- src/main/java/com/moaguide/domain/hanwoo/CattlePriceRepository.java
+++ src/main/java/com/moaguide/domain/hanwoo/CattlePriceRepository.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface CattlePriceRepository extends JpaRepository<CattlePrice, Long> {
-    @Query("SELECT new com.moaguide.dto.NewDto.customDto.CattleTransactionDto(cp.date, SUM(cp.value))" +
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.CattleTransactionDto(YEAR(cp.date), MONTH(cp.date), SUM(cp.value))" +
             "FROM CattlePrice cp " +
             "WHERE cp.date >= :date " +
             "GROUP BY YEAR(cp.date), MONTH(cp.date) " +
--- src/main/java/com/moaguide/dto/NewDto/customDto/CattleTransactionDto.java
+++ src/main/java/com/moaguide/dto/NewDto/customDto/CattleTransactionDto.java
@@ -1,5 +1,6 @@
 package com.moaguide.dto.NewDto.customDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
@@ -10,9 +11,9 @@ public class CattleTransactionDto {
     private LocalDate day;
     private Long  value;
 
-    public CattleTransactionDto(LocalDate date, Long  value) {
-        this.day = date;
+    public CattleTransactionDto(int year, int month, Long value) {
+        // 해당 년, 월의 1일을 LocalDate로 생성
+        this.day = LocalDate.of(year, month, 1);
         this.value = value;
     }
-
 }
