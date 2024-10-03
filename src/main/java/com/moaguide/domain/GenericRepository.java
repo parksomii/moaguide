b6914272854ca278f585ce333d1334f1fd5a6bab
LJH <qufwnsgh3@naver.com>
2024-10-03 18:32:13 +0900

    로그찍어보기 2

--- src/main/java/com/moaguide/domain/GenericRepository.java
+++ src/main/java/com/moaguide/domain/GenericRepository.java
@@ -61,7 +61,7 @@ public class GenericRepository {
     }
 
     public List<IssueCustomDto> findCustomStart(int page, int size ,Date sqlDate,String nickname) {
-        log.info("세팅값",page,size,sqlDate,nickname);
+        log.info("세팅값 - page: {}, size: {}, sqlDate: {}, nickname: {}", page, size, sqlDate, nickname);
         return entityManager.createNativeQuery("CALL start(:page, :size, :day,:nickname)", "IssueCustomDtoMapping")
                 .setParameter("page", page)
                 .setParameter("size", size)
