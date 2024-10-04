3c377c4740df7851e43971e66f56ccbdddf51161
chanho <jincanhao@naver.com>
2024-10-04 15:34:10 +0900

    사용하지 않는 파일 정리 1

--- src/main/java/com/moaguide/dto/NewDto/BuildingDto/LandRegistryDto.java
+++
@@ -1,4 +0,0 @@
-package com.moaguide.dto.NewDto.BuildingDto;
-
-public class LandRegistryDto {
-}
--- src/main/java/com/moaguide/dto/NewDto/customDto/SummaryGraphDto.java
+++
@@ -1,8 +0,0 @@
-package com.moaguide.dto.NewDto.customDto;
-
-import java.util.Date;
-
-public class SummaryGraphDto {
-    private String product_Id;
-    private Date date;
-}
