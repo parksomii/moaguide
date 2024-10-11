61d8f32f466b1ee7443662a1caa22cebb40fc3c3
LJH <qufwnsgh3@naver.com>
2024-10-11 23:16:26 +0900

    영화쪽 수정2

--- src/main/java/com/moaguide/domain/content/movie/MovieDetailRepository.java
+++ src/main/java/com/moaguide/domain/content/movie/MovieDetailRepository.java
@@ -3,13 +3,14 @@ package com.moaguide.domain.content.movie;
 
 import com.moaguide.dto.NewDto.customDto.MovieSubDto;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MovieDetailRepository {
+public interface MovieDetailRepository  extends JpaRepository<MovieDetail, Long> {
 
     @Query("select new com.moaguide.dto.NewDto.customDto.MovieSubDto(ms.screenCount,ms.day)  FROM MovieDetail ms where ms.productId.productId =:Id order by ms.day")
     List<MovieSubDto> findByScreen(String productId);
