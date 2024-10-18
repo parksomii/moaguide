fa1fa32b6990ddf26f32ef0d46a62f03012d51e8
LJH <qufwnsgh3@naver.com>
2024-10-18 16:11:41 +0900

    배열로보내기

--- src/main/java/com/moaguide/dto/NewDto/customDto/MovieInfoDto.java
+++ src/main/java/com/moaguide/dto/NewDto/customDto/MovieInfoDto.java
@@ -1,13 +1,13 @@
 package com.moaguide.dto.NewDto.customDto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class MovieInfoDto {
     // 영화소개
@@ -23,9 +23,24 @@ public class MovieInfoDto {
     // 감독
     private String director;
     // 출연
-    private String actor;
+    private List<String> actor;
     // 배급사
     private String distributor;
     // 원자정보
     private String originalInfo;
+
+    public MovieInfoDto(String movieInfo,String subgenre,Date releaseDate,String grade,Integer screeningTime,String director,String actor,String distributor,String originalInfo){
+        this.movieInfo = movieInfo;
+        this.subgenre = subgenre;
+        this.releaseDate = releaseDate;
+        this.grade = grade;
+        this.runningTime = screeningTime;
+        this.director = director;
+        this.distributor = distributor;
+        this.originalInfo = movieInfo;
+        this.actor = new ArrayList<>();
+        for (String actorString:actor.split(",")) {
+            this.actor.add(actorString.trim());
+        }
+    }
 }
