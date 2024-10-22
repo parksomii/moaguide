9fa530520c9d6a68d985393c89f6084f5fca34c2
LJH <qufwnsgh3@naver.com>
2024-10-22 19:52:02 +0900

    수정

--- src/main/java/com/moaguide/domain/building/businessarea/BusinessAreaRepository.java
+++ src/main/java/com/moaguide/domain/building/businessarea/BusinessAreaRepository.java
@@ -12,9 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public interface BusinessAreaRepository extends JpaRepository<BusinessArea, Long> {
 
-    @Query("SELECT new com.moaguide.dto.NewDto.BusinessAreaDto( b.cbd, b.cbdDistance , b.cbdCar , b.cbdSubway, b.gbd , b.gbdDistance , b.cbdCar , b.cbdSubway, b.ybd, b.ybdDistance, b.ybdCar, b.ybdSubway ) " +
+    @Query("SELECT new com.moaguide.dto.NewDto.BusinessAreaDto( b.cbd, b.cbdDistance , b.cbdCar , b.cbdSubway, b.gbd , b.gbdDistance , b.gbdCar , b.gbdSubway, b.ybd, b.ybdDistance, b.ybdCar, b.ybdSubway ) " +
             "FROM BusinessArea b,BuildingDetail bd " +
-            "WHERE bd.productId.productId =  b.productId.productId " +
+            "WHERE bd.productId.productId =  b.productId.productId and " +
             "b.productId.productId = :id ")
     BusinessAreaDto findByproductId(@Param("id") String product_Id);
 }
