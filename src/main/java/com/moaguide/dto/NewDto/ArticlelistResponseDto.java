46c887ac221a6ef8da71cbffd0364b5ebd87991e
LJH <qufwnsgh3@naver.com>
2024-10-23 14:10:53 +0900

    학습하기 수정

--- src/main/java/com/moaguide/dto/NewDto/ArticlelistResponseDto.java
+++ src/main/java/com/moaguide/dto/NewDto/ArticlelistResponseDto.java
@@ -17,8 +17,8 @@ public class ArticlelistResponseDto {
         this.articleList = article.getContent();
 
         // 10번째 요소의 id를 nextCursur에 할당, 없으면 null 처리
-        if (articleList.size() == 10) {
-            this.nextCursor = articleList.get(9).getId(); // 10번째 요소 (index 9)
+        if (articleList.size() == 20) {
+            this.nextCursor = articleList.get(19).getId(); // 10번째 요소 (index 9)
         } else {
             this.nextCursor = null; // 10번째 요소가 없으면 null 처리
         }
