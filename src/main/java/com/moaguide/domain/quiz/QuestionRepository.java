d8d9e149037023062bef777b28966b8365cc073f
LJH <qufwnsgh3@naver.com>
2024-11-04 19:03:31 +0900

    퀴즈 수정

--- src/main/java/com/moaguide/domain/quiz/QuestionRepository.java
+++ src/main/java/com/moaguide/domain/quiz/QuestionRepository.java
@@ -11,12 +11,12 @@ import java.util.List;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-    @Query("SELECT q FROM Question q where q.quizId = :id  ORDER BY CASE q.type WHEN 'b' THEN 1 WHEN 'c' THEN 2 WHEN 'a' THEN 3 END")
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.QuestionDto(q.explanation,q.choice1,q.choice2,q.choice3,q.choice4,q.choice5) FROM Question q where q.quizId = :id  ORDER BY CASE q.type WHEN 'b' THEN 1 WHEN 'c' THEN 2 WHEN 'a' THEN 3 END")
     List<QuestionDto> findByTypeB(@Param("id") Integer id);
 
-    @Query("SELECT q FROM Question q where q.quizId = :id  ORDER BY CASE q.type WHEN 'c' THEN 1 WHEN 'a' THEN 2 WHEN 'b' THEN 3 END")
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.QuestionDto(q.explanation,q.choice1,q.choice2,q.choice3,q.choice4,q.choice5) FROM Question q where q.quizId = :id  ORDER BY CASE q.type WHEN 'c' THEN 1 WHEN 'a' THEN 2 WHEN 'b' THEN 3 END")
     List<QuestionDto> findByTypeC(@Param("id") Integer id);
 
-    @Query("SELECT q FROM Question q where q.quizId = :id  ORDER BY CASE q.type WHEN 'a' THEN 1 WHEN 'b' THEN 2 WHEN 'c' THEN 3 END")
+    @Query("SELECT new com.moaguide.dto.NewDto.customDto.QuestionDto(q.explanation,q.choice1,q.choice2,q.choice3,q.choice4,q.choice5) FROM Question q where q.quizId = :id  ORDER BY CASE q.type WHEN 'a' THEN 1 WHEN 'b' THEN 2 WHEN 'c' THEN 3 END")
     List<QuestionDto> findByTypeA(@Param("id") Integer id);
 }
