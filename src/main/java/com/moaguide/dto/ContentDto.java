35ecc08f63d2674d70d88d988e78c09a421027a1
parksomii <somi020605@gmail.com>
2024-11-19 23:20:29 +0900

    feat: 학습하기 콘텐츠 기능 추가
    - Content 엔티티, 레포지토리, 서비스, 컨트롤러, DTO 구현
    - 콘텐츠와 카테고리 관계 설정

---
+++ src/main/java/com/moaguide/dto/ContentDto.java
@@ -0,0 +1,18 @@
+package com.moaguide.dto;
+
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ContentDto {
+    private int contentId;
+    private String title;
+    private String type;
+    private boolean isPremium;
+    private int views;
+    private Date date;
+    private int likes;
+    private int comments;
+}
