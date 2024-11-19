6cbd895bc7012dda96d51926f0405cd3222fff7f
parksomii <somi020605@gmail.com>
2024-11-19 22:39:24 +0900

    feat: 학습하기 카테고리 기능 추가
    - CategoryContent 엔티티, 레포지토리, 서비스, 컨트롤러, DTO 구현

---
+++ src/main/java/com/moaguide/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.moaguide.controller;
+
+import com.moaguide.dto.CategoryDto;
+import com.moaguide.service.CategoryService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> getAllCategories() {
+        List<CategoryDto> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+}
