844ed479b53a4fefbbf1a382ea572b42d1f48941
moaguide <qufwnsgh3@naver.com>
2024-12-02 22:57:42 +0900

    feat: develop과 메인 합체
    엘라스틱 추가
    
    develop 브런치 새로 만들 예정

---
+++ src/main/java/com/moaguide/domain/elasticsearch/products/ProductsRepository.java
@@ -0,0 +1,11 @@
+package com.moaguide.domain.elasticsearch.products;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface ProductsRepository extends ElasticsearchRepository<ProductsEntity, String> {
+    // Custom query methods can be added here
+}
