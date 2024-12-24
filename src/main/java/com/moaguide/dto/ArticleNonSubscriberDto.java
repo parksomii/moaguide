318c4a07383c1f2626890652f384d7296420ba7f
parksomii <somi020605@gmail.com>
2024-12-24 19:51:54 +0900

    feat: 비구독자용 아티클 정보를 위한 ArticleNonSubscriberDto 클래스 추가
    - 비구독자(USER)는 Paywall 상단 내용만 반환

---
+++ src/main/java/com/moaguide/dto/ArticleNonSubscriberDto.java
@@ -0,0 +1,18 @@
+package com.moaguide.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ArticleNonSubscriberDto {
+    private Long articleId;
+    private String categoryName;
+    private String title;
+    private String authorName;
+    private String paywallUp;
+    private String imgLink;
+    private Integer views;
+    private Integer likes;
+    private String createdAt;
+}
