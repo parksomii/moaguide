dbed05075b122c163f5d5918ac364ba94fd80c99
parksomii <somi020605@gmail.com>
2024-12-25 00:43:45 +0900

    refactor: Content와 Article_Content 테이블을 ArticleContent로 통합

--- src/main/java/com/moaguide/domain/ArticleContent/ArticleContent.java
+++ src/main/java/com/moaguide/domain/ArticleContent/ArticleContent.java
@@ -1,36 +1,61 @@
 package com.moaguide.domain.ArticleContent;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.sql.Timestamp;
+import com.moaguide.domain.CategoryContent.CategoryContent;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Entity
+import java.sql.Timestamp;
+
+@Builder
 @Getter
-@Table(name = "Content")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ArticleContent")
 public class ArticleContent {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "content_id")
-    private int contentId;
+    @Column(name = "article_id")
+    private Long articleId;
 
-    @Column(name = "category_id")
-    private int categoryId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private CategoryContent categoryId;
+
+    @Column(name = "author_name", nullable = false)
+    private String authorName;
+
+    @Column(name = "image_link", columnDefinition = "TEXT")
+    private String imageLink;
+
+    @Column(name = "title", columnDefinition = "TEXT", nullable = false)
     private String title;
+
+    @Column(name = "paywall_up", columnDefinition = "MEDIUMTEXT")
+    private String paywallUp;
+
+    @Column(name = "paywall_down", columnDefinition = "MEDIUMTEXT")
+    private String paywallDown;
+
+    @Column(name = "type", length = 50)
     private String type;
 
     @Column(name = "created_at")
     private Timestamp createdAt;
 
-    @Column(name = "is_premium")
-    private boolean isPremium;
+    @Column(name = "is_premium", columnDefinition = "TINYINT(1)")
+    private Boolean isPremium;
 
-    private int views;
-    private int likes;
-    private String description;
-    private String img_link;
+    @Column(name = "views")
+    private Integer views;
+
+    @Column(name = "likes")
+    private Integer likes;
+
+    @Column(name = "img_link", columnDefinition = "TEXT")
+    private String imgLink;
 }
