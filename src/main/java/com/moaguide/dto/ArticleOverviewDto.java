79f8a131d0c2a06f0c813c771defb5ce2907123a
parksomii <somi020605@gmail.com>
2024-12-29 02:47:54 +0900

    feat: 설명글 150글자 제한 로직 추가
    - ArticleContentRepository에 paywallUp 150글자 제한 로직 추가
    - 150자 초과 시 SUBSTRING으로 자르고 '...' 추가

--- src/main/java/com/moaguide/dto/ArticleOverviewDto.java
+++ src/main/java/com/moaguide/dto/ArticleOverviewDto.java
@@ -7,11 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ArticleOverviewDto {
 
-    private Long articleId;
-    private String title;
-    private String type;
-    private boolean isPremium;
-    private String paywallUp;
-    private String imgLink;
-    private String categoryName;
+  private Long articleId;
+  private String title;
+  private String type;
+  private boolean isPremium;
+  private String description;
+  private String imgLink;
+  private String categoryName;
 }
