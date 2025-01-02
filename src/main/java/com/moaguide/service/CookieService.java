44f04c7f659057ac590c8b16ca7b1d374c62279e
moaguide <qufwnsgh3@naver.com>
2025-01-02 17:42:09 +0900

    fix: 쿠키 도메인 localhost:3000으로 변경

--- src/main/java/com/moaguide/service/CookieService.java
+++ src/main/java/com/moaguide/service/CookieService.java
@@ -27,6 +27,7 @@ public class CookieService {
     public Cookie createLocalCookie(String key, String value, long maxAge) {
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge((int) (maxAge / 1000)); // 밀리초를 초 단위로 변환
+        cookie.setDomain("http://lcoalhost:3000");
         cookie.setHttpOnly(false);
         cookie.setSecure(false); // HTTPS 사용 시
         cookie.setPath("/");
@@ -57,6 +58,7 @@ public class CookieService {
     public Cookie createLocalRememberMeCookie(boolean rememberMe, long maxAge) {
         String value = Boolean.toString(rememberMe); // true 또는 false 값을 문자열로 변환
         Cookie cookie = new Cookie("rememberMe", value);
+        cookie.setDomain("http://lcoalhost:3000");
         cookie.setMaxAge((int) (maxAge / 1000)); // 밀리초를 초 단위로 변환
         cookie.setHttpOnly(false);
         cookie.setSecure(false); // HTTPS 사용 시
