ab5cc53a91601bb3fb7e3c71c5ca799f66d0b5b1
parksomii <somi020605@gmail.com>
2025-01-05 03:23:13 +0900

    feat: ArticleOverview에 사용자별 좋아요 여부 추가
    - 메인 홈에 좋아요 여부 추가

--- src/main/java/com/moaguide/service/ArticleContent/ArticleOverviewService.java
+++ src/main/java/com/moaguide/service/ArticleContent/ArticleOverviewService.java
@@ -16,27 +16,28 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ArticleOverviewService {
 
-  private final ArticleContentRepository articleContentRepository;
+	private final ArticleContentRepository articleContentRepository;
 
-  // 인기 콘텐츠 (조회수 기준 정렬)
-  public List<ArticleOverviewDto> getPopularContents(int popularLimit) {
-    Pageable pageable = PageRequest.of(0, popularLimit, Sort.by(Sort.Direction.DESC, "views"));
-    return articleContentRepository.findContentsByViews(pageable)
-        .getContent();
-  }
+	// 인기 콘텐츠 (조회수 기준 정렬)
+	public List<ArticleOverviewDto> getPopularContents(int popularLimit) {
+		Pageable pageable = PageRequest.of(0, popularLimit, Sort.by(Sort.Direction.DESC, "views"));
+		return articleContentRepository.findContentsByViews(pageable)
+			.getContent();
+	}
 
-  // 최신 콘텐츠 (최신 날짜 기준 정렬)
-  public List<ArticleOverviewDto> getRecentContents(int recentLimit) {
-    Pageable pageable = PageRequest.of(0, recentLimit, Sort.by(Sort.Direction.DESC, "createdAt"));
-    return articleContentRepository.findContentsWithCategory(pageable)
-        .getContent();
-  }
+	// 최신 콘텐츠 (최신 날짜 기준 정렬)
+	public List<ArticleOverviewDto> getRecentContents(int recentLimit) {
+		Pageable pageable = PageRequest.of(0, recentLimit,
+			Sort.by(Sort.Direction.DESC, "createdAt"));
+		return articleContentRepository.findContentsWithCategory(pageable)
+			.getContent();
+	}
 
-  // 최신 뉴스 클리핑 (최신 날짜 기준 정렬)
-  public List<ArticleOverviewDto> getLatestEconomicIssues(Category category, int newsLimit) {
-    Pageable pageable = PageRequest.of(0, newsLimit, Sort.by(Sort.Direction.DESC, "createdAt"));
+	// 최신 뉴스 클리핑 (최신 날짜 기준 정렬)
+	public List<ArticleOverviewDto> getNewsContents(Category category, int newsLimit) {
+		Pageable pageable = PageRequest.of(0, newsLimit, Sort.by(Sort.Direction.DESC, "createdAt"));
 
-    return articleContentRepository.findByCategory(category.getId(), pageable)
-        .getContent();
-  }
+		return articleContentRepository.findByCategory(category.getId(), pageable)
+			.getContent();
+	}
 }
