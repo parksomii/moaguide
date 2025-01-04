bc5c4b48101af4af738fc83e4c4ba926cb6cbc1a
parksomii <somi020605@gmail.com>
2025-01-04 16:46:35 +0900

    refactor: 아티클 작성 시 카테고리 ID에서 Name으로 변경
    - ArticleContentWriteService에서 카테고리 조회 방식 변경: id → name

--- src/main/java/com/moaguide/service/ArticleContent/ArticleContentWriteService.java
+++ src/main/java/com/moaguide/service/ArticleContent/ArticleContentWriteService.java
@@ -14,52 +14,54 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ArticleContentWriteService {
 
-  private final ArticleContentRepository articleContentRepository;
-  private final CategoryContentRepository categoryContentRepository;
+	private final ArticleContentRepository articleContentRepository;
+	private final CategoryContentRepository categoryContentRepository;
 
-  /**
-   * 아티클 저장
-   *
-   * @param requestDto 아티클 요청 DTO
-   */
-  public void saveArticle(ArticleContentWriteRequestDto requestDto) {
-    // 1. CategoryContent 조회
-    CategoryContent categoryContent = categoryContentRepository.findById(requestDto.getCategoryId())
-        .orElseThrow(() -> new IllegalArgumentException("해당 categoryId에 대한 카테고리를 찾을 수 없습니다."));
+	/**
+	 * 아티클 저장
+	 *
+	 * @param requestDto 아티클 요청 DTO
+	 */
+	public void saveArticle(ArticleContentWriteRequestDto requestDto) {
+		// 1. CategoryContent 조회
+		CategoryContent categoryContent = categoryContentRepository.findByName(
+				requestDto.getCategoryName())
+			.orElseThrow(
+				() -> new IllegalArgumentException("해당 categoryName에 대한 카테고리를 찾을 수 없습니다."));
 
-    // 2. ArticleContent 생성
-    ArticleContent articleContent = ArticleContent.builder()
-        .title(requestDto.getTitle())
-        .authorName(requestDto.getAuthorName())
-        .categoryId(categoryContent)
-        .type(requestDto.getType())
-        .createdAt(Timestamp.valueOf(LocalDateTime.now()))
-        .isPremium(requestDto.isPremium())
-        .views(0) // 초기 조회수 설정
-        .imgLink(requestDto.getImageLink())
-        .paywallUp(requestDto.getPaywallUp())
-        .paywallDown(requestDto.getPaywallDown())
-        .build();
+		// 2. ArticleContent 생성
+		ArticleContent articleContent = ArticleContent.builder()
+			.title(requestDto.getTitle())
+			.authorName(requestDto.getAuthorName())
+			.categoryId(categoryContent)
+			.type(requestDto.getType())
+			.createdAt(Timestamp.valueOf(LocalDateTime.now()))
+			.isPremium(requestDto.isPremium())
+			.views(0) // 초기 조회수 설정
+			.imgLink(requestDto.getImageLink())
+			.paywallUp(requestDto.getPaywallUp())
+			.paywallDown(requestDto.getPaywallDown())
+			.build();
 
-    // 3. 저장
-    articleContentRepository.save(articleContent);
-  }
+		// 3. 저장
+		articleContentRepository.save(articleContent);
+	}
 
-  /**
-   * 아티클 삭제
-   *
-   * @param articleId 삭제할 아티클 ID
-   * @return 삭제 성공 여부
-   */
-  public boolean deleteArticleById(Long articleId) {
-    // 1. articleId로 ArticlePaywall 확인
-    if (!articleContentRepository.existsById(articleId)) {
-      return false; // 삭제할 아티클이 존재하지 않음
-    }
+	/**
+	 * 아티클 삭제
+	 *
+	 * @param articleId 삭제할 아티클 ID
+	 * @return 삭제 성공 여부
+	 */
+	public boolean deleteArticleById(Long articleId) {
+		// 1. articleId로 ArticlePaywall 확인
+		if (!articleContentRepository.existsById(articleId)) {
+			return false; // 삭제할 아티클이 존재하지 않음
+		}
 
-    // 2. 삭제
-    articleContentRepository.deleteById(articleId);
-    return true;
-  }
+		// 2. 삭제
+		articleContentRepository.deleteById(articleId);
+		return true;
+	}
 
 }
