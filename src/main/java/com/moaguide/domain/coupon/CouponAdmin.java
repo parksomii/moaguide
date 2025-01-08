6022ea9cd7e2986c91191c06e65c52c6eccda6f3
parksomii <somi020605@gmail.com>
2025-01-08 16:59:44 +0900

    fix: CouponAdmin,  CouponUser 필드 매핑 오류 수정

--- src/main/java/com/moaguide/domain/coupon/CouponAdmin.java
+++ src/main/java/com/moaguide/domain/coupon/CouponAdmin.java
@@ -1,35 +1,41 @@
 package com.moaguide.domain.coupon;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Entity
 @Table(
-        name = "Coupon_Admin",
-        uniqueConstraints = @UniqueConstraint(columnNames = {"couponCode", "nickname"}) // UNIQUE 제약 조건 설정
+	name = "Coupon_Admin",
+	uniqueConstraints = @UniqueConstraint(columnNames = {"coupon_Code",
+		"nickname"}) // UNIQUE 제약 조건 설정
 )
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class CouponAdmin {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // Primary Key
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // Primary Key
 
-    private String name; // 쿠폰 이름
+	private String name; // 쿠폰 이름
 
-    @Column(name = "coupon_code")
-    private String couponCode; // 쿠폰 고유 번호
+	@Column(name = "coupon_code")
+	private String couponCode; // 쿠폰 고유 번호
 
-    @Column(name="created_at")
-    private LocalDate createdAt; // 발행일
+	@Column(name = "created_at")
+	private LocalDate createdAt; // 발행일
 
-    private Integer months; // 쿠폰 적용 기간 (개월수)
+	private Integer months; // 쿠폰 적용 기간 (개월수)
 
-    private String nickname; // User와 외래 키로 연결
+	private String nickname; // User와 외래 키로 연결
 }
--- src/main/java/com/moaguide/domain/coupon/CouponUser.java
+++ src/main/java/com/moaguide/domain/coupon/CouponUser.java
@@ -1,39 +1,45 @@
 package com.moaguide.domain.coupon;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Entity
 @Table(
-        name = "Coupon_User",
-        uniqueConstraints = @UniqueConstraint(columnNames = {"nickname", "couponId"}) // 닉네임과 쿠폰 ID 조합에 대한 UNIQUE 제약 조건
+	name = "Coupon_User",
+	uniqueConstraints = @UniqueConstraint(columnNames = {"nickname",
+		"coupon_Id"}) // 닉네임과 쿠폰 ID 조합에 대한 UNIQUE 제약 조건
 )
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class CouponUser {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // Primary Key
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // Primary Key
 
-    private String nickname; // 사용자 닉네임 (외래 키로 사용)
+	private String nickname; // 사용자 닉네임 (외래 키로 사용)
 
-    @Column(name = "coupon_Id", nullable = false)
-    private Long couponId; // 쿠폰 ID (외래 키로 사용)
+	@Column(name = "coupon_Id", nullable = false)
+	private Long couponId; // 쿠폰 ID (외래 키로 사용)
 
-    private Boolean redeemed = false; // 쿠폰 사용 여부, 기본값 FALSE
+	private Boolean redeemed = false; // 쿠폰 사용 여부, 기본값 FALSE
 
-    @Column(name = "redeemed_At")
-    private LocalDate redeemedAt; // 쿠폰 사용 날짜 (NULL 가능)
+	@Column(name = "redeemed_At")
+	private LocalDate redeemedAt; // 쿠폰 사용 날짜 (NULL 가능)
 
-    public CouponUser(String nickname, Long couponId, Boolean redeemed) {
-        this.nickname = nickname;
-        this.couponId = couponId;
-        this.redeemed = redeemed;
-    }
+	public CouponUser(String nickname, Long couponId, Boolean redeemed) {
+		this.nickname = nickname;
+		this.couponId = couponId;
+		this.redeemed = redeemed;
+	}
 }
