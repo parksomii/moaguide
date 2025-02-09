edc1eb73e95331657140a0e818a6474d609ddb76
moaguide <qufwnsgh3@naver.com>
2025-02-09 22:25:16 +0900

    feat: 아티클 이미지 API 수정

--- src/main/java/com/moaguide/controller/ArticleContent/ArticleImageRestController.java
+++ src/main/java/com/moaguide/controller/ArticleContent/ArticleImageRestController.java
@@ -1,18 +1,10 @@
 package com.moaguide.controller.ArticleContent;
 
 import com.moaguide.service.ArticleContent.ArticleImageService;
-import com.moaguide.service.FileService;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @AllArgsConstructor
