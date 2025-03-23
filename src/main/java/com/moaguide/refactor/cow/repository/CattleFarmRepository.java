2e8adbcb106adeb45f330675b4236a7f6fa2c294
kch12 <gaemkyu4@gmail.com>
2025-03-23 17:22:56 +0900

    rename: cowmarket repository 경로 수정

--- src/main/java/com/moaguide/refactor/cow/repository/CattleFarmRepository.java
+++ src/main/java/com/moaguide/refactor/cow/repository/CattleFarmRepository.java
@@ -1,6 +1,7 @@
-package com.moaguide.refactor.cow.entity;
+package com.moaguide.refactor.cow.repository;
 
 import com.moaguide.dto.NewDto.customDto.CattleFarmDto;
+import com.moaguide.refactor.cow.entity.CattleFarm;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
