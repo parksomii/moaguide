7e9ff9b72aadfbc5613026f7f1d129685c210189
parksomii <somi020605@gmail.com>
2025-03-23 18:17:05 +0900

    rename: art 부분 controller 경로 수정

--- src/main/java/com/moaguide/refactor/art/controller/ArtRestController.java
+++ src/main/java/com/moaguide/refactor/art/controller/ArtRestController.java
@@ -1,16 +1,19 @@
-package com.moaguide.controller;
+package com.moaguide.refactor.art.controller;
 
 import com.moaguide.dto.NewDto.ArtBaseResponseDto;
 import com.moaguide.dto.NewDto.ArtDetailDto;
 import com.moaguide.refactor.security.jwt.JWTUtil;
 import com.moaguide.service.art.ArtService;
+import java.util.HashMap;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
