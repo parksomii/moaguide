022a825af8f00ecab43432fb1fb7adb625e6777c
parksomii <somi020605@gmail.com>
2025-03-23 17:09:18 +0900

    fix: Local 사용 시 불필요한 부분 제거

--- src/main/java/com/moaguide/config/LocalSecurityConfig.java
+++ src/main/java/com/moaguide/config/LocalSecurityConfig.java
@@ -5,12 +5,11 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.moaguide.config.handler.CustomAccessDeniedHandler;
 import com.moaguide.config.handler.CustomLogoutSuccessHandler;
-import com.moaguide.config.handler.OAuth2SuccessHandler;
 import com.moaguide.jwt.JWTFilter;
 import com.moaguide.jwt.JWTUtil;
 import com.moaguide.security.LocalLoginFilter;
 import com.moaguide.service.CookieService;
-import com.moaguide.service.CustomOAuth2UserService;
+import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,101 +30,100 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 @Profile("local")
 public class LocalSecurityConfig {
 
-    private final AuthenticationConfiguration authenticationConfiguration;
-    private final JWTUtil jwtUtil;
-    private final CookieService cookieService;
-    private final CustomOAuth2UserService customOAuth2UserService;
+	private final AuthenticationConfiguration authenticationConfiguration;
+	private final JWTUtil jwtUtil;
+	private final CookieService cookieService;
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+		throws Exception {
 
-        return configuration.getAuthenticationManager();
-    }
+		return configuration.getAuthenticationManager();
+	}
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(cors -> {
-                    cors.configurationSource(corsConfigurationzSource());
-                });
-        //csrf disable
-        http
-                .csrf((auth) -> auth.disable());
-        //From 로그인 방식 disable
-        http
-                .formLogin((auth) -> auth.disable());
-        //http basic 인증 방식 disable
-        http
-                .httpBasic((auth) -> auth.disable());
-        http.
-                addFilterBefore(new CorsFilter(corsConfigurationzSource()), ChannelProcessingFilter.class);
-        http
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.cors(cors -> {
+				cors.configurationSource(corsConfigurationzSource());
+			});
+		//csrf disable
+		http
+			.csrf((auth) -> auth.disable());
+		//From 로그인 방식 disable
+		http
+			.formLogin((auth) -> auth.disable());
+		//http basic 인증 방식 disable
+		http
+			.httpBasic((auth) -> auth.disable());
+		http.
+			addFilterBefore(new CorsFilter(corsConfigurationzSource()),
+				ChannelProcessingFilter.class);
+		http
 
-                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers("/logout", "/user/update/nickname").authenticated() // 특정 경로에 대해서만 인증 필요
-                        .anyRequest().permitAll()
-                );
-        // 특정 경로에 대해서만 JWTFilter 적용
-        http.
-                addFilterAt(new LocalLoginFilter(authenticationManager(authenticationConfiguration),jwtUtil,cookieService), UsernamePasswordAuthenticationFilter.class);
-        http
-                .oauth2Login(oauth2 -> oauth2
-                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService) // 사용자 정보 가져오기
-                        )
-                        .successHandler(new OAuth2SuccessHandler(jwtUtil,cookieService)) // OAuth2 성공 핸들러 직접 호출
-                );
-        http.
-                sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http
-                .logout(logout -> logout
-                        .logoutUrl("/logout") // 로그아웃 요청 URL
-                        .deleteCookies("JSESSIONID", "refresh", "rememberMe")
-                        .logoutSuccessHandler(new CustomLogoutSuccessHandler()) // 로그아웃 성공 후 메시지 반환
-                );
-        return http.build();
-    }
+			.addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+			.authorizeHttpRequests(request -> request
+				.requestMatchers("/logout", "/user/update/nickname")
+				.authenticated() // 특정 경로에 대해서만 인증 필요
+				.anyRequest().permitAll()
+			);
+		// 특정 경로에 대해서만 JWTFilter 적용
+		http.
+			addFilterAt(
+				new LocalLoginFilter(authenticationManager(authenticationConfiguration), jwtUtil,
+					cookieService), UsernamePasswordAuthenticationFilter.class);
+		http.
+			sessionManagement((session) -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		http
+			.logout(logout -> logout
+				.logoutUrl("/logout") // 로그아웃 요청 URL
+				.deleteCookies("JSESSIONID", "refresh", "rememberMe")
+				.logoutSuccessHandler(new CustomLogoutSuccessHandler()) // 로그아웃 성공 후 메시지 반환
+			);
+		return http.build();
+	}
 
-    private AccessDeniedHandler accessDeniedHandler() {
-        return  new CustomAccessDeniedHandler();
-    }
+	private AccessDeniedHandler accessDeniedHandler() {
+		return new CustomAccessDeniedHandler();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    // JSON 변환을 위한 ObjectMapper
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        // Java 8 날짜/시간 모듈 등록
-        mapper.registerModule(new JavaTimeModule());
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        return mapper;
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationzSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000","*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); // 허용 메서드
-        configuration.setAllowedHeaders(Arrays.asList("Authorization","verify", "Content-Type","cookie")); // 허용 헤더
-        configuration.setExposedHeaders(Arrays.asList("Authorization","verify")); // 클라이언트가 접근할 수 있는 응답 헤더
-        configuration.setAllowCredentials(true); // 자격증명 허용
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+	// JSON 변환을 위한 ObjectMapper
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		// Java 8 날짜/시간 모듈 등록
+		mapper.registerModule(new JavaTimeModule());
+		mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		return mapper;
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationzSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "*"));
+		configuration.setAllowedMethods(
+			Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); // 허용 메서드
+		configuration.setAllowedHeaders(
+			Arrays.asList("Authorization", "verify", "Content-Type", "cookie")); // 허용 헤더
+		configuration.setExposedHeaders(
+			Arrays.asList("Authorization", "verify")); // 클라이언트가 접근할 수 있는 응답 헤더
+		configuration.setAllowCredentials(true); // 자격증명 허용
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 
 }
