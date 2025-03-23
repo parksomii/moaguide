941949398a58cf5d750951063b4ec4887cc7df00
parksomii <somi020605@gmail.com>
2025-03-23 17:26:37 +0900

    rename: art 부분 entity, repository 경로 수정

--- src/main/java/com/moaguide/refactor/art/repository/ArtDetailRepository.java
+++ src/main/java/com/moaguide/refactor/art/repository/ArtDetailRepository.java
@@ -1,6 +1,8 @@
-package com.moaguide.domain.detail;
+package com.moaguide.refactor.art.repository;
 
+import com.moaguide.refactor.art.entity.ArtDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArtDetailRepository extends JpaRepository<ArtDetail, Long> {
+
 }
