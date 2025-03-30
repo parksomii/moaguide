e45b3e2d04cfd8de791b4843991083221e8f41a3
parksomii <somi020605@gmail.com>
2025-03-30 13:48:21 +0900

    refactor: TransactionService 중복 코드 개선

--- src/main/java/com/moaguide/refactor/product/service/TransactionService.java
+++ src/main/java/com/moaguide/refactor/product/service/TransactionService.java
@@ -1,18 +1,17 @@
 package com.moaguide.refactor.product.service;
 
-import com.moaguide.refactor.product.repository.HistoryRepository;
-import com.moaguide.refactor.product.repository.TransactionRepository;
 import com.moaguide.refactor.product.dto.DetailTransactionResponseDto;
 import com.moaguide.refactor.product.dto.TransactionDto;
+import com.moaguide.refactor.product.repository.HistoryRepository;
+import com.moaguide.refactor.product.repository.TransactionRepository;
 import jakarta.persistence.EntityManager;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
@@ -24,7 +23,6 @@ public class TransactionService {
 	private final EntityManager entityManager;
 
 	public DetailTransactionResponseDto findbymonth(String productId, int month) {
-		List<TransactionDto> transactionDtos = new ArrayList<>();
 		if (month <= 12) {
 
 			LocalDate day = LocalDate.now().minusMonths(month);
@@ -35,27 +33,7 @@ public class TransactionService {
 				.setParameter(2, day)  // 변수 day 사용
 				.getResultList();
 			if (results.size() > 0) {
-
-				long maxValue = (Long) results.get(0)[1];
-				long minValue = (Long) results.get(0)[1];
-
-				for (Object[] result : results) {
-					LocalDate tradeDay = ((Date) result[0]).toLocalDate();
-					long price = ((Number) result[1]).longValue();
-
-					TransactionDto dto = new TransactionDto(tradeDay, price);
-					transactionDtos.add(dto);
-
-					// 최대값과 최소값 계산
-					if (price > maxValue) {
-						maxValue = price;
-					}
-					if (price < minValue) {
-						minValue = price;
-					}
-				}
-
-				return new DetailTransactionResponseDto(transactionDtos, maxValue, minValue);
+				return minMaxCal(results);
 			}
 		} else if (month == 100) {
 			LocalDate day = LocalDate.now().minusMonths(month);
@@ -67,25 +45,6 @@ public class TransactionService {
 				.setParameter(2, day)  // 변수 day 사용
 				.getResultList();
 			if (results.size() > 0) {
-				long maxValue = (Long) results.get(0)[1];
-				long minValue = (Long) results.get(0)[1];
-
-				for (Object[] result : results) {
-					LocalDate tradeDay = ((Date) result[0]).toLocalDate();
-					long price = ((Number) result[1]).longValue();
-
-					TransactionDto dto = new TransactionDto(tradeDay, price);
-					transactionDtos.add(dto);
-
-					// 최대값과 최소값 계산
-					if (price > maxValue) {
-						maxValue = price;
-					}
-					if (price < minValue) {
-						minValue = price;
-					}
-				}
-
 				// 히스토리 테이블에서 데이터를 가져옵니다.
 				List<Object[]> historyResults = entityManager.createNativeQuery(
 						"SELECT h.trade_day, h.price FROM History h WHERE h.product_id = ?1 AND h.trade_day >= ?2  order by h.trade_day desc")
@@ -93,28 +52,38 @@ public class TransactionService {
 					.setParameter(2, day)
 					.getResultList();
 				if (historyResults.size() > 0) {
-
-					// 히스토리 테이블의 결과를 TransactionDto로 변환하고 병합
-					for (Object[] result : historyResults) {
-						LocalDate tradeDay = ((Date) result[0]).toLocalDate();
-						long price = ((Number) result[1]).longValue();
-
-						TransactionDto dto = new TransactionDto(tradeDay, price);
-						transactionDtos.add(dto);
-
-						// 최대값과 최소값 계산
-						if (price > maxValue) {
-							maxValue = price;
-						}
-						if (price < minValue) {
-							minValue = price;
-						}
-					}
-
+					results.addAll(historyResults);
+					return minMaxCal(results);
+				} else {
+					return minMaxCal(results);
 				}
-				return new DetailTransactionResponseDto(transactionDtos, maxValue, minValue);
 			}
 		}
 		return new DetailTransactionResponseDto(null, 0, 0);
+	}
+
+	private DetailTransactionResponseDto minMaxCal(List<Object[]> results) {
+		List<TransactionDto> transactionDtos = new ArrayList<>();
+
+		long maxValue = (Long) results.get(0)[1];
+		long minValue = (Long) results.get(0)[1];
+
+		for (Object[] result : results) {
+			LocalDate tradeDay = ((Date) result[0]).toLocalDate();
+			long price = ((Number) result[1]).longValue();
+
+			TransactionDto dto = new TransactionDto(tradeDay, price);
+			transactionDtos.add(dto);
+
+			// 최대값과 최소값 계산
+			if (price > maxValue) {
+				maxValue = price;
+			}
+			if (price < minValue) {
+				minValue = price;
+			}
+		}
+
+		return new DetailTransactionResponseDto(transactionDtos, maxValue, minValue);
 	}
 }
