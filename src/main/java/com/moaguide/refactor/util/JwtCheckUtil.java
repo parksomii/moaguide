8d9ec12b404377f4af185f48b0fd76e17dd26820
parksomii <somi020605@gmail.com>
2025-03-30 15:13:11 +0900

    feat: JwtCheckUtil에 닉네임 추출 메서드 추가

--- src/main/java/com/moaguide/refactor/util/JwtCheckUtil.java
+++ src/main/java/com/moaguide/refactor/util/JwtCheckUtil.java
@@ -1,6 +1,5 @@
 package com.moaguide.refactor.util;
 
-
 import com.moaguide.refactor.jwt.util.JwtUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -8,9 +7,21 @@ import org.springframework.stereotype.Component;
 @Component
 @AllArgsConstructor
 public class JwtCheckUtil {
+
 	private final JwtUtil jwtUtil;
 
-	public static boolean jwtCheck(String token) {
+	public String extractNickname(String token) {
+		if (token == null || !token.startsWith("Bearer ")) {
+			return "null";
+		}
 
+		String accessToken = token.substring(7);
+
+		// 만료되었을 경우 null로 처리
+		if (jwtUtil.isExpired(accessToken)) {
+			return null;
+		}
+
+		return jwtUtil.getNickname(accessToken);
 	}
 }
